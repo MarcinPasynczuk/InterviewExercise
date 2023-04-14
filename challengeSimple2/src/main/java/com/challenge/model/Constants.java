101ab431c323f3e23aabe161d6744d74bfe14f41
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2023-04-14 12:48:05 +0200

    Delete challengeSimple2 directory

--- challengeSimple2/src/main/java/com/challenge/model/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.challenge.model;
-
-public class Constants {
-	 public static final String SUCCESS = "success";
-	 public static final String PAGENOTFOUND = "404";
-	 public static final String REGISTRATION= "registration";
-	 public static final String INDEX = "index";
-}
